9458bf567682c78f1e1c028c3368f19f256dd2dc
Ranim Alsalloum <ranim.alsalloum@hmmh.de>
2025-10-23 11:42:02 +0200

    POST /api/projects Endpoint mit grundlegender Validierung hinzugefügt

---
+++ src/main/java/de/szut/lf8_starter/project/ProjectsService.java
@@ -0,0 +1,11 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.project.dto.ProjectCreateDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectsService {
+    public void createProject(ProjectCreateDto projectCreateDto) {
+
+    }
+}
